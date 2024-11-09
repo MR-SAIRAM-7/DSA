00a529dbadba13a20e349e4ba69ad6abc2a0b6ce
mr-sairam-7 <mr.sairamofficial@gmail.com>
2024-11-09 11:15:02 +0530

    patterns

---
+++ Day 06 Patterns/Main.java
@@ -0,0 +1,89 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Printing different Patterns
+        // *
+        // * *
+        // * * *
+        // * * * *
+        // {
+        //     int n = 4;
+        //     for (int i = 0; i < n; i++) {
+        //         for (int j = 0; j <= i; j++) {
+        //             System.out.print("* ");
+        //         }
+        //         System.out.println();
+        //     }
+        // }
+        // * * * * 
+        // * * *
+        // * *
+        // *
+        // {
+        //     int n = 4;
+        //     for(int i=0;i<n;i++){
+        //         for(int j=n;j>i;j--){
+        //             System.out.print("* ");
+        //         }
+        //         System.out.println();
+        //     }
+        // }
+        // 1
+        // 12
+        // 123
+        // 1234
+        // {
+        // int n = 4;
+        // for (int i = 0; i < n; i++) {
+        //     int num = 1;
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print(num);
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+        // int n = 4;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j <= i; j++) {
+        //         System.out.print(j + 1);
+        //     }
+        //     System.out.println();
+        // }
+        // }
+        // A
+        // BC
+        // DEF
+        // GHIJ
+        // {
+        //     int n = 4;
+        //     char ch = 'A';
+        //     for (int i = 0; i < n; i++) {
+        //         for (int j = 0; j <= i; j++) {
+        //             System.out.print(ch);
+        //             ch++;
+        //         }
+        //         System.out.println();
+        //     }
+        // }
+
+
+        // A
+        // AB
+        // ABC
+        // ABCD
+        // {
+        //     int n = 4;
+        //     for (int i = 0; i < n; i++) {
+        //         char ch = 'A';
+        //         for (int j = 0; j <= i; j++) {
+        //             System.out.print(ch);
+        //             ch++;
+        //         }
+        //         System.out.println();
+        //     }
+        // }
+
+    }
+}
